4bb49a1e7b191686fb5008fd4f3a13031bfc65f0
pietrolatorre <pietro.latorre@quantyca.it>
2016-10-13 18:10:07 +0200

    uno

--- tutorial-kafka-producer/src/main/resources/runKafkaProducer.sh
+++ tutorial-kafka-producer/src/main/resources/runKafkaProducer.sh
@@ -4,16 +4,21 @@
 #  this is an example script the will require edits to make it work in any
 #  environment.
 ###############################################################################
-HOST="servername.domain.internal"
+#HOST="servername.domain.internal"
 ## THIS IS CLUSTER/PLATFORM DEPENDENT
 # MapR or HDP
-SPLICE_LIB_DIR="/opt/splice/default/lib"
-KAFKA_LIB_DIR="/opt/kafka/default/libs"
+#SPLICE_LIB_DIR="/opt/splice/default/lib"
+#KAFKA_LIB_DIR="/opt/kafka/default/libs"
 # Cloudera
 # SPLICE_LIB_DIR="/opt/cloudera/parcels/SPLICEMACHINE/lib"
 # KAFKA_LIB_DIR="/opt/cloudera/parcels/KAFKA/lib/kafka/libs/"
 
-SPLICE_JAR="${SPLICE_LIB_DIR}/splice-tutorial-kafka-producer-2.0.jar"
+HOST="clouderadev2.c.hadoop-144807.internal"
+SPLICE_LIB_DIR="/opt/cloudera/parcels/SPLICEMACHINE-2.0.1.28.cdh5.8.0.p0.159/lib/"
+KAFKA_LIB_DIR="/opt/cloudera/parcels/KAFKA-2.0.2-1.2.0.2.p0.5/lib/kafka/libs/"
+
+
+SPLICE_JAR="${SPLICE_LIB_DIR}/splice-tutorial-kafka-producer-2.0.1.18.jar"
 APPENDSTRING=`echo ${KAFKA_LIB_DIR}/*.jar | sed 's/ /:/g'`
 
 java -cp ${SPLICE_JAR}:${APPENDSTRING} \
--- tutorial-kafka-spark-streaming/src/main/resources/scripts/run-kafka-spark-streaming.sh
+++ tutorial-kafka-spark-streaming/src/main/resources/scripts/run-kafka-spark-streaming.sh
@@ -7,11 +7,11 @@
 ## THIS IS CLUSTER/PLATFORM DEPENDENT
 # MapR
 # https://community.mapr.com/docs/DOC-1396
-SPLICE_LIB_DIR="/opt/splice/default/lib"
-KAFKA_LIB_DIR="/opt/kafka/default/libs"
-export SPARK_HOME="/opt/mapr/spark/spark-1.6.1"
-export HADOOP_CONF_DIR=/opt/mapr/hadoop/hadoop-2.7.0/etc/hadoop/
-export LD_LIBRARY_PATH=/opt/mapr/hadoop/hadoop-2.7.0/lib/native/:${LD_LIBRARY_PATH}
+#SPLICE_LIB_DIR="/opt/splice/default/lib"
+#KAFKA_LIB_DIR="/opt/kafka/default/libs"
+#export SPARK_HOME="/opt/mapr/spark/spark-1.6.1"
+#export HADOOP_CONF_DIR=/opt/mapr/hadoop/hadoop-2.7.0/etc/hadoop/
+#export LD_LIBRARY_PATH=/opt/mapr/hadoop/hadoop-2.7.0/lib/native/:${LD_LIBRARY_PATH}
 
 # Cloudera
 # SPLICE_LIB_DIR="/opt/cloudera/parcels/SPLICEMACHINE/lib"
@@ -19,18 +19,28 @@ export LD_LIBRARY_PATH=/opt/mapr/hadoop/hadoop-2.7.0/lib/native/:${LD_LIBRARY_PA
 # export SPARK_HOME="/opt/cloudera/parcels/CDH/lib/spark"
 # export HADOOP_CONF_DIR=/etc/hadoop/conf
 
+SPLICE_LIB_DIR="/opt/cloudera/parcels/SPLICEMACHINE-2.0.1.28.cdh5.8.0.p0.159/lib/"
+KAFKA_LIB_DIR="/opt/cloudera/parcels/KAFKA-2.0.2-1.2.0.2.p0.5/lib/kafka/libs/"
+export SPARK_HOME="/opt/cloudera/parcels/CDH/lib/spark"
+export HADOOP_CONF_DIR=/etc/hadoop/conf
 
 CLASS_NAME="com.splicemachine.tutorials.sparkstreaming.kafka.SparkStreamingKafka"
 export ADDITIONAL_JARS="${SPLICE_LIB_DIR}/spark-streaming-kafka*.jar,${KAFKA_LIB_DIR}/kafka-clients-*.jar"
+#export APPLICATION_JAR="${SPLICE_LIB_DIR}/splice-tutorial-kafka-spark-streaming-2.0.1.18.jar"
+
+export ADDITIONAL_JARS1="${KAFKA_LIB_DIR}/kafka-clients-*.jar"
+export ADDITIONAL_JARS2="${SPLICE_LIB_DIR}/spark-streaming-kafka*.jar"
+
 export APPLICATION_JAR="${SPLICE_LIB_DIR}/splice-tutorial-kafka-spark-streaming-2.0.1.18.jar"
 
 exec "${SPARK_HOME}"/bin/spark-submit \
   --name TutorialKafka \
   --master yarn \
   --deploy-mode cluster \
-  --executor-memory 2G \
+  --executor-memory 256M \
   --num-executors 3 \
   --class ${CLASS_NAME} \
-  --jars ${ADDITIONAL_JARS} \
+  --jars ${ADDITIONAL_JARS1} \
+  --jars ${ADDITIONAL_JARS2} \
   "${APPLICATION_JAR}" \
   "$@"
