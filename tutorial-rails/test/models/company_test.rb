bb722a6039a6c47e41a6295a32d43df58be2faaa
Kolosek <office@kolosek.com>
2016-10-19 13:50:14 +0200

    Fixed the rails app to be run out of the box and updated readme file

--- tutorial-rails/test/models/company_test.rb
+++ tutorial-rails/test/models/company_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class CompanyTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Company can be created" do
+    company = Company.create(name: 'Company Name')
+    assert_equal company.name, 'Company Name'
+  end
 end
